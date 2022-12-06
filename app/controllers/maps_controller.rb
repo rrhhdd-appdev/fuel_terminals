ff575b303a7557af24d7b867c5cebbe3845273ca
rrhhdd <rharrisondavis@chicagobooth.edu>
2022-12-06 00:54:44 +0000

    Google Maps page progress (all terminals showing)

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,10 @@
 class MapsController < ApplicationController
   
   def index
+    matching_facilities = Facility.all
+
+    @list_of_facilities = matching_facilities.order({ :created_at => :desc })
+    
     render({ :template => "maps/index.html.erb" })
   end
 end
